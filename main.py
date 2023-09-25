80fc350df4d092ca6381b752ffc794774a56bc74
Khazixi <khazixfangirl@gmail.com>
2023-09-25 00:15:30 -0400

    Stores base stats to database, missing difficulty based stats

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from bs4 import BeautifulSoup, BeautifulStoneSoup
+from bs4 import BeautifulSoup
 import requests
+import sqlite3
 from pprint import pprint
 
 basestats_URL = "https://serenesforest.net/awakening/characters/base-stats/main-story/"
@@ -7,5 +8,51 @@ basestats_URL = "https://serenesforest.net/awakening/characters/base-stats/main-
 basestats_page = requests.get(basestats_URL)
 
 soup = BeautifulSoup(basestats_page.content, "html.parser")
+con = sqlite3.connect("awakening.db")
+cur = con.cursor()
 
-pprint(soup)
+def sql():
+    cur.execute(
+        """
+            CREATE TABLE IF NOT EXISTS
+            basestats(
+                name TEXT,
+                class TEXT,
+                level INTEGER,
+                hp INTEGER,
+                str INTEGER,
+                mag INTEGER,
+                skl INTEGER,
+                spd INTEGER,
+                lck INTEGER,
+                def INTEGER,
+                res INTEGER,
+                mov INTEGER
+            )
+        """
+    )
+
+def bases():
+    for element in soup.find_all('tr'):
+        b = [x.get_text() for x in element.find_all('td', limit=12)]
+        if (b and len(b) == 12):
+            data = cur.execute(
+                """
+                    INSERT INTO basestats VALUES(
+                        ?, ?, ?,
+                        ?, ?, ?,
+                        ?, ?, ?,
+                        ?, ?, ?
+                    )
+                """,
+                b
+            )
+            print(data.description)
+            for d in data:
+                print(d)
+        else:
+            print('Fucked Up')
+    con.commit()
+
+sql()
+bases()
