58bff5f3417d4dddaec55fa14fd9086b3a792c88
Khazixi <khazixfangirl@gmail.com>
2023-09-25 12:31:58 -0400

    Added Base Growths and asset stats

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 import sqlite3
-from pprint import pprint
 
 basestats_URL = "https://serenesforest.net/awakening/characters/base-stats/main-story/"
+basegrowths_URL = "https://serenesforest.net/awakening/characters/growth-rates/base/"
 
-basestats_page = requests.get(basestats_URL)
-
-soup = BeautifulSoup(basestats_page.content, "html.parser")
 con = sqlite3.connect("awakening.db")
 cur = con.cursor()
 
-def sql():
+
+def schema():
     cur.execute(
         """
             CREATE TABLE IF NOT EXISTS
@@ -31,8 +29,43 @@ def sql():
             )
         """
     )
+    cur.execute(
+        """
+            CREATE TABLE IF NOT EXISTS
+            basegrowths(
+                name TEXT,
+                hp INTEGER,
+                str INTEGER,
+                mag INTEGER,
+                skl INTEGER,
+                spd INTEGER,
+                lck INTEGER,
+                def INTEGER,
+                res INTEGER
+            )
+        """
+    )
+    cur.execute(
+        """
+            CREATE TABLE IF NOT EXISTS
+            asset(
+                asset TEXT,
+                hp TEXT,
+                str TEXT,
+                mag TEXT,
+                skl TEXT,
+                spd TEXT,
+                lck TEXT,
+                def TEXT,
+                res TEXT
+            )
+        """
+    )
 
-def bases():
+
+def base_stats():
+    basestats_page = requests.get(basestats_URL)
+    soup = BeautifulSoup(basestats_page.content, "html.parser")
     for element in soup.find_all('tr'):
         b = [x.get_text() for x in element.find_all('td', limit=12)]
         if (b and len(b) == 12):
@@ -54,5 +87,57 @@ def bases():
             print('Fucked Up')
     con.commit()
 
-sql()
-bases()
+
+def base_growths():
+    basegrowths_page = requests.get(basegrowths_URL)
+    soup = BeautifulSoup(basegrowths_page.content, "html.parser")
+    b = soup.find(string="Asset/Flaw").find_parent("table").find_all("tr")
+    for element in b:
+        c = [x.get_text() for x in element.find_all("td")]
+        if (len(c) == 9):
+            cur.execute(
+                """
+                    INSERT INTO asset VALUES(
+                        ?, ?, ?,
+                        ?, ?, ?,
+                        ?, ?, ?
+                    )
+                """,
+                c
+            )
+
+    d = soup.find(string="Chrom").find_parent("table").find_all("tr")
+    for element in d:
+        c = [x.get_text() for x in element.find_all("td")]
+        if (len(c) == 9):
+            cur.execute(
+                """
+                    INSERT INTO basegrowths VALUES(
+                        ?, ?, ?,
+                        ?, ?, ?,
+                        ?, ?, ?
+                    )
+                """,
+                c
+            )
+
+    e = soup.find(string="Lucina").find_parent("table").find_all("tr")
+    for element in e:
+        c = [x.get_text() for x in element.find_all("td")]
+        if (len(c) == 9):
+            cur.execute(
+                """
+                    INSERT INTO basegrowths VALUES(
+                        ?, ?, ?,
+                        ?, ?, ?,
+                        ?, ?, ?
+                    )
+                """,
+                c
+            )
+    con.commit()
+
+
+schema()
+# base_stats()
+base_growths()
