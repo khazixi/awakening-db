da9d96f645a316862ac14f81760b75113231685b
Khazixi <khazixfangirl@gmail.com>
2023-10-16 19:02:01 -0400

    Made DB Script Interactable, you can select only what you need

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from bs4 import BeautifulSoup
+from typing import Callable
 import re
 import requests
 import sqlite3
+import pytermgui as ptg
+import os
+
+
+# NOTE:
+# using pytermgui for a terminal gui
+# (I have no idea how I would make this a text-base cli)
+# using click for arg parsing (There should not be too much)?
+
+# TODO: investigate packaging solutions for this script?
+
+# TODO: Add colorful prompts to make it a proper CLI?
+# TODO: Add ability to check off features wanted for creating database?
+# TODO: Add parameterization of connection and cursor to Functions?
+
 
 basestats_URL = "https://serenesforest.net/awakening/characters/base-stats/main-story/"
 basegrowths_URL = "https://serenesforest.net/awakening/characters/growth-rates/base/"
@@ -10,12 +26,12 @@ class_base_URL = "https://serenesforest.net/awakening/classes/base-stats/"
 skills_URL = "https://serenesforest.net/awakening/miscellaneous/skills/"
 character_assets_URL = "https://serenesforest.net/awakening/characters/maximum-stats/modifiers/"
 
-con = sqlite3.connect("awakening.db")
-cur = con.cursor()
+# TODO: Make these parameters so that the db can be created as a script
+# con = sqlite3.connect("awakening.db")
+# cur = con.cursor()
 
 
-def schema():
-    # TODO: Refactor into one string?
+def schema(cur: sqlite3.Cursor, con: sqlite3.Connection):
     cur.executescript(
         """
             CREATE TABLE IF NOT EXISTS
@@ -106,7 +122,7 @@ def schema():
     )
 
 
-def base_stats():
+def base_stats(cur: sqlite3.Cursor, con: sqlite3.Connection):
     basestats_page = requests.get(basestats_URL)
     soup = BeautifulSoup(basestats_page.content, "html.parser")
     for element in soup.find_all('tr'):
@@ -148,8 +164,8 @@ def split_affinity(c: list[str]) -> tuple[list[str], list[str]]:
     return (asset, flaw)
 
 
-# TODO: Change assets to strings instead of numbers
-def base_growths():
+# NOTE: Types kind of don't matter because of the way SQLite works
+def base_growths(cur: sqlite3.Cursor, con: sqlite3.Connection):
     basegrowths_page = requests.get(basegrowths_URL)
     soup = BeautifulSoup(basegrowths_page.content, "html.parser")
     b = soup.find(string="Magic").find_parent("table").find_all("tr")
@@ -179,7 +195,6 @@ def base_growths():
                 """,
                 e
             )
-
 
     d = soup.find(string="Chrom").find_parent("table").find_all("tr")
     for element in d:
@@ -213,7 +228,7 @@ def base_growths():
     con.commit()
 
 
-def class_sets():
+def class_sets(cur: sqlite3.Cursor, con: sqlite3.Connection):
     page = requests.get(class_URL)
     soup = BeautifulSoup(page.content, "html.parser")
     classes = soup.find(string="Regular classes").find_parent(
@@ -230,7 +245,7 @@ def class_sets():
     con.commit()
 
 
-def class_base():
+def class_base(cur: sqlite3.Cursor, con: sqlite3.Connection):
     page = requests.get(class_base_URL)
     soup = BeautifulSoup(page.content, "html.parser")
     classes = soup.find_all("tr")
@@ -251,7 +266,7 @@ def class_base():
     con.commit()
 
 
-def class_skills():
+def class_skills(cur: sqlite3.Cursor, con: sqlite3.Connection):
     page = requests.get(skills_URL)
     soup = BeautifulSoup(page.content, "html.parser")
     classes = soup.find("table").find_all("tr")
@@ -270,8 +285,8 @@ def class_skills():
     con.commit()
 
 
-# TODO: Change assets to strings instead of numbers
-def character_assets():
+# NOTE: Types kind of don't matter because of the way SQLite works
+def character_assets(cur: sqlite3.Cursor, con: sqlite3.Connection):
     page = requests.get(character_assets_URL)
     soup = BeautifulSoup(page.content, "html.parser")
     classes = soup.find(string="Magic").find_parent("table").find_all("tr")
@@ -303,7 +318,8 @@ def character_assets():
                 e
             )
 
-    parent_assets = soup.find(string="Chrom").find_parent("table").find_all("tr")
+    parent_assets = soup.find(string="Chrom").find_parent(
+        "table").find_all("tr")
     for element in parent_assets:
         a = [x.get_text() for x in element.find_all("td")]
         if (len(a) == 8):
@@ -320,10 +336,105 @@ def character_assets():
     con.commit()
 
 
-schema()
+# schema()
 # base_stats()
 # base_growths()
 # class_sets()
 # class_base()
 # class_skills()
-character_assets()
+# character_assets()
+
+# INFO: Doing this because I don't know how to do pass by reference in Python
+actions: list[Callable] = []
+
+
+def toggle_function(fn: Callable):
+    if fn in actions:
+        actions.remove(fn)
+    else:
+        actions.append(fn)
+
+
+def get_file():
+    if os.path.isfile("awakening.db"):
+        return "Modify DB"
+    else:
+        return "Create DB"
+
+
+def runner(x):
+    try:
+        file = open("awakening.db", "x")
+        file.close()
+    except FileExistsError:
+        pass
+
+    con = sqlite3.connect("awakening.db")
+    cur = con.cursor()
+
+    schema(cur, con)
+
+    for action in actions:
+        action(cur, con)
+
+    exit(0)
+
+
+with ptg.WindowManager() as manager:
+    window = (
+        ptg.Window(
+            "",
+            ptg.Splitter(
+                ptg.Label("Base Stats"),
+                ptg.Checkbox(
+                    tates=("X", "O"),
+                    checked=toggle_function(base_stats)
+                )
+            ),
+            ptg.Splitter(
+                ptg.Label("Base Growths"),
+                ptg.Checkbox(
+                    states=("X", "O"),
+                    checked=toggle_function(base_growths)
+                )
+            ),
+            ptg.Splitter(
+                ptg.Label("Class Sets"),
+                ptg.Checkbox(
+                    states=("X", "O"),
+                    checked=toggle_function(class_sets)
+                )
+            ),
+            ptg.Splitter(
+                ptg.Label("Class Base"),
+                ptg.Checkbox(
+                    states=("X", "O"),
+                    checked=toggle_function(class_base)
+                )
+            ),
+            ptg.Splitter(
+                ptg.Label("Class Skills"),
+                ptg.Checkbox(
+                    states=("X", "O"),
+                    checked=toggle_function(class_skills)
+                )
+            ),
+            ptg.Splitter(
+                ptg.Label("Character Assetts"),
+                ptg.Checkbox(
+                    states=("X", "O"),
+                    checked=toggle_function(character_assets)
+                )
+            ),
+            ptg.Label(""),
+            ptg.Label(""),
+            ptg.Button(
+                get_file(),
+                onclick=runner,
+            ),
+        )
+        .set_title("[210 bold]Fire Emblem Awakening Database Generator")
+        .center()
+    )
+
+    manager.add(window)
